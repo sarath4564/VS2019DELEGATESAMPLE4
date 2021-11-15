fc4c6ae08d426bd7c6e27ba05baa01c8596be758
SARATHCHANDRA <sarathchandra76@gmail.com>
2021-11-15 14:21:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+// delegate void MyDelegate(int x, int y);
+//   class Program
+//   {
+//        static void Main()
+//        {
+//            MyDelegate del  = Division;
+//            del(9000,3000);
+//        }
+//                static void Division (int x,int y)
+//                {
+//                    Console.WriteLine(x/y);
+//                }
+//   }
+
+
+using System;
+delegate void MyDelegate(int x, int y);
+class Program
+{
+    static void Main()
+    {
+        MyDelegate del = Division;
+        del(9000, 3000);
+    }
+    static void Division(int x, int y)
+    {
+        Console.WriteLine(x / y);
+    }
+}
+
